184c808e76086b3ca33e24a27257d7415a983480
Velo <hartley.max@outlook.com>
2020-11-23 15:55:23 +0000

    boilerplate added to social component. import and rendered in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import '../css/app.css';
 import Landing from './Landing';
+import Social from './Social';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<Router>
 			<div className='app'>
+				<Social />
 				<Switch>
 					<Route path='/'>
 						<Landing />
