c88ee73b7b79d9ed7be89a49bead773fcd639cca
Velo <hartley.max@outlook.com>
2020-11-23 16:32:20 +0000

    github logo added replaced text

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/social.css';
+import githubLogo from '../images/GitHub-Mark-32px.png';
 
 export default function Social() {
 	return (
 		<nav>
-			<a href='https://github.com/Velocima'>github</a>
+			<a href='https://github.com/Velocima'>
+				<img src={githubLogo} alt='' />
+			</a>
 			<a href='mailto:'>mail</a>
 		</nav>
 	);
