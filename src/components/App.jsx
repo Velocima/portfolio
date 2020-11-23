c0ba16b582bb163c80eb807f994766b9c7738129
Velo <hartley.max@outlook.com>
2020-11-23 15:50:39 +0000

    basic content added to landing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import '../css/app.css';
 import Landing from './Landing';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className='app'>
-			<Landing />
-		</div>
+		<Router>
+			<div className='app'>
+				<Switch>
+					<Route path='/'>
+						<Landing />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Landing() {
-	return <main>landing</main>;
+	return (
+		<main>
+			<h1>Hi, I'm Max.</h1>
+			<h2>A Full Stack Developer</h2>
+			<p>
+				Checkout <Link to='/projects'>My Projects</Link>
+			</p>
+		</main>
+	);
 }
