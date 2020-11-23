b9a60f8b0b30c47a1c69f3364f7c8954410c79e6
Velo <hartley.max@outlook.com>
2020-11-23 16:48:52 +0000

    mail padded and replaced with icon from react-icons. react-icons added as dependency

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/social.css';
 import githubLogo from '../images/GitHub-Mark-32px.png';
+import { IconContext } from 'react-icons';
+import { BiEnvelope } from 'react-icons/bi';
 
 export default function Social() {
 	return (
@@ -8,7 +10,11 @@ export default function Social() {
 			<a href='https://github.com/Velocima'>
 				<img src={githubLogo} alt='' />
 			</a>
-			<a href='mailto:'>mail</a>
+			<a href='mailto:'>
+				<IconContext.Provider value={{ className: 'react-icons' }}>
+					<BiEnvelope />
+				</IconContext.Provider>
+			</a>
 		</nav>
 	);
 }
