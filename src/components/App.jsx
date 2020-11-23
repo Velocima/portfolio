fc207bc398b588f9c9ed190838caa12298ffa305
Velo <hartley.max@outlook.com>
2020-11-23 15:15:54 +0000

    boilerplate added to landing component. imported into app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import '../css/app.css';
+import Landing from './Landing';
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Landing />
+		</div>
+	);
 }
 
 export default App;
