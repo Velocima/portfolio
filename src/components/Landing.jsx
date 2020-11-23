5e75250e20e5f73509b65cec041db8165c3d346b
Velo <hartley.max@outlook.com>
2020-11-23 16:22:52 +0000

    initial styling added to landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,7 +5,9 @@ import '../css/landing.css';
 export default function Landing() {
 	return (
 		<main>
-			<h1>Hi, I'm Max.</h1>
+			<h1>
+				Hi, I'm <span>Max</span>
+			</h1>
 			<h2>A Full Stack Developer</h2>
 			<p>
 				Checkout <Link to='/projects'>My Projects</Link>
