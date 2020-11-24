ca4f4a806ad489f931463b698c6ae0390a1ade30
Velo <hartley.max@outlook.com>
2020-11-24 13:30:43 +0000

    initial content added to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { BiLink } from 'react-icons/bi';
+import maxLandingImg from '../images/max-landing-page.png';
+import millicentLandingImg from '../images/millicent-landing-page.png';
 
 export default function Projects() {
-	return <main>Projects</main>;
+	return (
+		<main>
+			<article>
+				<img src={maxLandingImg} alt='The landing page for my portfolio website' />
+				<a href='https://github.com/Velocima/portfolio' target='_blank'>
+					<IconContext.Provider value={{ className: 'react-icons' }}>
+						Code <BiLink />
+					</IconContext.Provider>
+				</a>
+				<a href='https://maxhartley.com/' target='_blank'>
+					Live version <BiLink />
+				</a>
+			</article>
+			<article>
+				<img src={millicentLandingImg} alt='The landing page for Millicent Mehndi henna' />
+				<a href='https://github.com/Velocima/Millicent' target='_blank'>
+					<IconContext.Provider value={{ className: 'react-icons' }}>
+						Code <BiLink />
+					</IconContext.Provider>
+				</a>
+				<a href='http://www.millicent-mehndi.co.uk/' target='_blank'>
+					Live version <BiLink />
+				</a>
+			</article>
+		</main>
+	);
 }
